0f994b67a36d1c4e4fea54d6e7d9bb3728a907b0
U'Day <malwaremamu@gmail.com>
2023-04-17 14:40:37 -0400

    Add files via upload

---
+++ RoktSDK/Sources/RoktSDK/RoktSDK.swift
@@ -0,0 +1,19 @@
+public struct RoktSDK {
+    
+    public func average(nums: [Int]) -> Int {
+
+        var total = 0.0
+        //use the parameter-array instead of the global variable votes
+        for vote in nums{
+            total += Double(vote)
+        }
+
+        let votesTotal = Double(nums.count)
+        let average = total/votesTotal
+        if average > 0 {
+            return Int(average)
+        } else {
+            return 0
+        }
+    }
+}
---
+++ RoktSDK/Tests/RoktSDKTests/RoktSDKTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import RoktSDK
+
+final class RoktSDKTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(RoktSDK().text, "Hello, World!")
+    }
+}
